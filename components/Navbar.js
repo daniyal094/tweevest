c4c6ea6a4d27c59000c50939ba46340458f86486
Daniyal Ahmed <daniyal.ahmed@ordercall.com>
2022-08-30 01:37:41 +0500

    Navbar cmp clear

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,175 +1,7 @@
-// import { useState } from "react";
-// import Image from "next/image";
-// function NavLink({ to, children }) {
-//   return (
-//     <a href={to} className={`mx-4 text-blue-900 text-sm font-medium`}>
-//       {children}
-//     </a>
-//   );
-// }
-
-// function MobileNav({ open, setOpen }) {
-//   return (
-//     <div
-//       className={`absolute top-16 left-0  w-screen bg-gray-100 transform ${
-//         open ? "-translate-x-0" : "-translate-x-full"
-//       } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
-//     >
-
-//       <div className="flex flex-col ml-4 mt-10">
-//         <a
-//           className="text-xl font-medium my-4  text-blue-900 "
-//           href="/"
-//           onClick={() =>
-//             setTimeout(() => {
-//               setOpen(!open);
-//             }, 100)
-//           }
-//         >
-//            Screener
-
-//         </a>
-//         <a
-//           className="text-xl font-normal my-4 text-blue-900 "
-//           href="/"
-//           onClick={() =>
-//             setTimeout(() => {
-//               setOpen(!open);
-//             }, 100)
-//           }
-//         >
-//           Watchlist
-//         </a>
-//         <a
-//           className="text-xl font-normal my-4 text-blue-900 "
-//           href="/"
-//           onClick={() =>
-//             setTimeout(() => {
-//               setOpen(!open);
-//             }, 100)
-//           }
-//         >
-//           About
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default function Navbar() {
-//   const [open, setOpen] = useState(false);
-//   return (
-//     <nav className="flex filter  bg-gray-100 px-4 h-16 items-center">
-//       <MobileNav open={open} setOpen={setOpen} />
-//       <div className="w-5/12 flex items-center">
-//         <a className="text-2xl font-semibold" href="/">
-//           <Image alt="" src={"/logo.svg"} width={150} height={30} />
-//         </a>
-//       </div>
-//       <div className="w-9/12 flex justify-end items-center">
-//       <div className="bg-white rounded flex-row flex items-center h-10 justify-center w-5/6 mr-10">
-//           <Image alt="" src={'/searchIcon.svg'} width={20} height={20} />
-//           <input className="bg-white outline-0 px-2   w-10/12 " placeholder="Search  Company Status"/>
-//         </div>
-//         <div
-//           className="z-50 flex relative w-8 h-8 flex-col justify-between items-center md:hidden"
-//           onClick={() => {
-//             setOpen(!open);
-//           }}
-//         >
-//           {/* hamburger button */}
-//           <span
-//             className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
-//               open ? "rotate-45 translate-y-3.5" : ""
-//             }`}
-//           />
-//           <span
-//             className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
-//               open ? "w-0" : "w-full"
-//             }`}
-//           />
-//           <span
-//             className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
-//               open ? "-rotate-45 -translate-y-3.5" : ""
-//             }`}
-//           />
-//         </div>
-
-//         <div className="hidden md:flex  items-center">
-//           <NavLink to="/">Screener</NavLink>
-//           <NavLink to="/">Watchlist</NavLink>
-//           <NavLink to="/">About</NavLink>
-//           <button className="bg-blue-500 text-white px-4 py-2 rounded">
-//             {" "}
-//             Download
-//           </button>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
 import Image from "next/image";
-import { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 
-// Profile Dropdown
-// const ProfileDropDown = (props) => {
-//   const [state, setState] = useState(false);
-//   const profileRef = useRef();
-
-//   const navigation = [
-//     { title: "Dashboard", path: "javascript:void(0)" },
-//     { title: "Settings", path: "javascript:void(0)" },
-//     { title: "Log out", path: "javascript:void(0)" },
-//   ];
-
-//   useEffect(() => {
-//     const handleDropDown = (e) => {
-//       if (!profileRef.current.contains(e.target)) setState(false);
-//     };
-//     document.addEventListener("click", handleDropDown);
-//   }, []);
-
-//   return (
-//     <div className={`relative ${props.class}`}>
-//       <div className="flex items-center space-x-4">
-//         <button
-//           ref={profileRef}
-//           className="w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 ring-2 lg:focus:ring-indigo-600"
-//           onClick={() => setState(!state)}
-//         >
-//           <img
-//             src="https://randomuser.me/api/portraits/men/46.jpg"
-//             className="w-full h-full rounded-full"
-//           />
-//         </button>
-//         <div className="lg:hidden">
-//           <span className="block">Micheal John</span>
-//           <span className="block text-sm text-gray-500">john@gmail.com</span>
-//         </div>
-//       </div>
-//       <ul
-//         className={`bg-white top-12 right-0 mt-5 space-y-5 lg:absolute lg:border lg:rounded-md lg:text-sm lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${
-//           state ? "" : "lg:hidden"
-//         }`}
-//       >
-//         {navigation.map((item, idx) => (
-//           <li>
-//             <a
-//               key={idx}
-//               className="block text-gray-600 lg:hover:bg-gray-50 lg:p-2.5"
-//               href={item.path}
-//             >
-//               {item.title}
-//             </a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-export default  Navbar = () => {
+const Navbar = () => {
   const [menuState, setMenuState] = useState(false);
 
   // Replace javascript:void(0) path with your path
@@ -184,12 +16,12 @@ export default  Navbar = () => {
       <div className="flex items-center space-x-8 py-3 px-4 max-w-screen-xl mx-auto">
         <div className="flex-none lg:flex-initial">
           <a href="javascript:void(0)">
-            <Image  src={"/logosvg.svg"} width={120} height={50} alt="logo" />
+            <Image src={"/logosvg.svg"} width={120} height={50} alt="logo" />
           </a>
         </div>
         <div className="flex-1 flex items-center justify-between">
           <div className="flex-1 flex items-center justify-end space-x-2 sm:space-x-6 ">
-            <form className="flex items-center w-full space-x-2 border rounded-md p-2 bg-white" >
+            <form className="flex items-center w-full space-x-2 border rounded-md p-2 bg-white">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5 flex-none text-gray-300"
@@ -218,7 +50,9 @@ export default  Navbar = () => {
               <ul className="mt-12 space-y-5 lg:flex lg:space-x-6 lg:space-y-0 lg:mt-0 justify-end">
                 {navigation.map((item, idx) => (
                   <li key={idx} className="text-gray-600 hover:text-gray-900">
-                    <p className={`mx-4 text-blue-900 text-sm font-medium cursor-pointer`}>
+                    <p
+                      className={`mx-4 text-blue-900 text-sm font-medium cursor-pointer`}
+                    >
                       {item.title}
                     </p>
                   </li>
@@ -269,3 +103,4 @@ export default  Navbar = () => {
     </nav>
   );
 };
+export default Navbar;
